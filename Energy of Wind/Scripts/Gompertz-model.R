518cff9b760237fe34fc1a353f64b0b3ccd31dc4
Guan, Changhong <cguan8@gatech.edu>
2022-07-02 19:55:57 -0400

    added gompertz

--- Energy of Wind/Scripts/Gompertz-model.R
+++ Energy of Wind/Scripts/Gompertz-model.R
@@ -22,30 +22,31 @@ data_germany_wind = data_germany[!mask_na & mask_has, model_col]
 newdata = data.frame(year = 2060, country='Germany',wind_electricity=380)
 data_germany_wind = rbind(data_germany_wind, newdata)
 
-# Logistic model, Asym/(1+exp((xmid-input)/scal))
-y.low <- nls(wind_electricity ~ SSlogis(year, Asym, xmid, scal), data=data_germany_wind)
-summary(y.low)
-Asym = coef(y.low)[1]
-xmid.mid = coef(y.low)[2]-1.3
-scal.mid = coef(y.low)[3]/1.3
-x = c(2022:2080)
-y.mid = Asym/(1+exp((xmid.mid-x)/scal.mid))
+# Gompertz model
 
-xmid.high = coef(y.low)[2]-2
-scal.high = coef(y.low)[3]/2
-y.high = Asym/(1+exp((xmid.high-x)/scal.high))
+data.g = data.frame(cbind(data_germany_wind$year-1986, data_germany_wind$wind_electricity))
+germany.gomp <- nls(X2 ~ SSgompertz(X1, Asym, xmid, scal), data=data.g)
+summary(germany.gomp)
+Asym = coef(germany.gomp)[1]
+xmid = coef(germany.gomp)[2]
+scal.low = coef(germany.gomp)[3]
+scal.mid = coef(germany.gomp)[3]/1.04
+scal.high = coef(germany.gomp)[3]/1.1
+x = c(1986:2080)-1986
+y.low = Asym*exp(-xmid*scal.low^x)
+y.mid = Asym*exp(-xmid*scal.mid^x)
+y.high = Asym*exp(-xmid*scal.high^x)
 
 setwd('..')
-png(file = paste(getwd(),'/Visualizations/logistic-model-germany.png', sep=""), width=600, height=600)
+png(file = paste(getwd(),'/Visualizations/Gompertz-model-germany.png', sep=""), width=600, height=600)
 
 plot(wind_electricity~year, data_germany_wind,
      xlab='Year', xlim = range(1985,2060),
      ylab='Electricity generation from wind, TWh', ylim = range(0, 400),
      main='Projections of Electricity Generation from wind, Germany')
-
-curve(predict(y.low, newdata = data.frame(year=x)), add=TRUE, col='green', lty = 2, lwd=2)
-lines(x,y.mid, col='orange', lty = 2, lwd=2)
-lines(x,y.high, col='red', lty = 2, lwd=2)
+lines(x+1986,y.low, col='green', lty = 2, lwd=2)
+lines(x+2001,y.mid, col='orange', lty = 2, lwd=2)
+lines(x+2009,y.high, col='red', lty = 2, lwd=2)
 abline(h=390, col='purple', lwd=3)
 abline(v=2035, col='blue', lwd=3)
 legend(1986, 350, legend=c("Logistic low prediction", "Logistic mid prediction", 'Logistic high prediction'),
@@ -62,32 +63,31 @@ data_poland_wind = data_poland[!mask_na & mask_has, model_col]
 newdata = data.frame(year = 2070, country='Poland',wind_electricity=66)
 data_poland_wind = rbind(data_poland_wind, newdata)
 
-y.low <- nls(wind_electricity ~ SSlogis(year, Asym, xmid, scal), data=data_poland_wind)
-summary(y.low)
-Asym = coef(y.low)[1]
-xmid.mid = coef(y.low)[2]-1.3
-scal.mid = coef(y.low)[3]/1.3
-x = c(2022:2080)
-y.mid = Asym/(1+exp((xmid.mid-x)/scal.mid))
+data.p = data.frame(cbind(data_poland_wind$year-2001, data_poland_wind$wind_electricity))
+poland.gomp <- nls(X2 ~ SSgompertz(X1, Asym, xmid, scal), data=data.p)
+summary(poland.gomp)
+Asym = coef(poland.gomp)[1]
+xmid = coef(poland.gomp)[2]
+scal.low = coef(poland.gomp)[3]
+scal.mid = coef(poland.gomp)[3]/1.04
+scal.high = coef(poland.gomp)[3]/1.1
+x = c(2001:2080)-2001
+y.low = Asym*exp(-xmid*scal.low^x)
+y.mid = Asym*exp(-xmid*scal.mid^x)
+y.high = Asym*exp(-xmid*scal.high^x)
 
-xmid.high = coef(y.low)[2]-2
-scal.high = coef(y.low)[3]/2
-y.high = Asym/(1+exp((xmid.high-x)/scal.high))
 
-png(file = paste(getwd(),'/Visualizations/logistic-model-poland.png', sep=""), width=600, height=600)
+png(file = paste(getwd(),'/Visualizations/Gompertz-model-poland.png', sep=""), width=600, height=600)
+
 plot(wind_electricity~year, data_poland_wind,
      xlab='Year', xlim = range(2000,2070),
      ylab='Electricity generation from wind, TWh', ylim = range(0, 70),
      main='Projections of Electricity Generation from wind, Poland')
-
-curve(predict(y.low, newdata = data.frame(year=x)), add=TRUE, col='green', lty = 2, lwd=2)
-lines(x,y.mid, col='orange', lty = 2, lwd=2)
-lines(x,y.high, col='red', lty = 2, lwd=2)
+lines(x+2001,y.low, col='green', lty = 2, lwd=2)
+lines(x+2008,y.mid, col='orange', lty = 2, lwd=2)
+lines(x+2012,y.high, col='red', lty = 2, lwd=2)
 abline(h=67, col='purple', lwd=3)
 abline(v=2035, col='blue', lwd=3)
-legend(2040, 20, legend=c("Logistic low prediction", "Logistic mid prediction", 'Logistic high prediction'),
+legend(2040, 10, legend=c("Logistic low prediction", "Logistic mid prediction", 'Logistic high prediction'),
        col=rep(c('green', 'orange', 'red'),1), lty=rep(2,3), cex=0.8)
 dev.off()
-
-
-
